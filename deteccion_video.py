37fbe1b931b29d059cc240c6da2e81a7cd59c74f
Bryant <bryant@MacBook-Pro-de-Bryant.local>
2020-09-20 09:03:58 -0500

    Mi primer commit

---
+++ deteccion_video.py
@@ -0,0 +1,98 @@
+from __future__ import division
+from models import *
+from utils.utils import *
+from utils.datasets import *
+import os
+import sys
+import argparse
+import cv2
+from PIL import Image
+import torch
+from torch.autograd import Variable
+from flask import Flask, render_template, Response
+import time
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    """Video streaming home page."""
+    return render_template('index.html')
+
+
+
+def Convertir_RGB(img):
+    # Convertir Blue, green, red a Red, green, blue
+    b = img[:, :, 0].copy()
+    g = img[:, :, 1].copy()
+    r = img[:, :, 2].copy()
+    img[:, :, 0] = r
+    img[:, :, 1] = g
+    img[:, :, 2] = b
+    return img
+
+
+def Convertir_BGR(img):
+    # Convertir red, blue, green a Blue, green, red
+    r = img[:, :, 0].copy()
+    g = img[:, :, 1].copy()
+    b = img[:, :, 2].copy()
+    img[:, :, 0] = b
+    img[:, :, 1] = g
+    img[:, :, 2] = r
+    return img
+
+def gen():
+    #cap = cv2.VideoCapture('http://admin:tello123@192.168.1.5:8080/video')
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = Darknet("config/yolov3-custom8C.cfg", img_size=416).to(device)
+    model.load_state_dict(torch.load("checkpoints/yolov3_ckpt_849.pth"))
+    model.eval()  
+    classes = load_classes("custom/classes.names")
+    Tensor = torch.cuda.FloatTensor if torch.cuda.is_available() else torch.FloatTensor
+    cap= cv2.VideoCapture(0)
+    colors = np.random.randint(0, 255, size=(len(classes), 3), dtype="uint8")
+    a=[]
+    while cap:
+        ret, frame = cap.read()
+        if ret is False:
+            break  
+        frame = cv2.resize(frame, (0,0), fx=0.5, fy=0.5,interpolation=cv2.INTER_CUBIC)
+        #LA imagen viene en Blue, Green, Red y la convertimos a RGB que es la entrada que requiere el modelo
+        RGBimg=Convertir_RGB(frame)
+        imgTensor = transforms.ToTensor()(RGBimg)
+        imgTensor, _ = pad_to_square(imgTensor, 0)
+        imgTensor = resize(imgTensor, 416)
+        imgTensor = imgTensor.unsqueeze(0)
+        imgTensor = Variable(imgTensor.type(Tensor))
+
+
+        with torch.no_grad():
+            detections = model(imgTensor)
+            detections = non_max_suppression(detections, 0.8, 0.4)
+
+        for detection in detections:
+            if detection is not None:
+                detection = rescale_boxes(detection, 416, RGBimg.shape[:2])
+                for x1, y1, x2, y2, conf, cls_conf, cls_pred in detection:
+                    box_w = x2 - x1
+                    box_h = y2 - y1
+                    color = [int(c) for c in colors[int(cls_pred)]]
+                    print("Se detectó {} en X1: {}, Y1: {}, X2: {}, Y2: {}".format(classes[int(cls_pred)], x1, y1, x2, y2))
+                    frame = cv2.rectangle(frame, (x1, y1 + box_h), (x2, y1), color, 5)
+                    cv2.putText(frame, classes[int(cls_pred)], (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 1, color, 5)# Nombre de la clase detectada
+                    cv2.putText(frame, str("%.2f" % float(conf)), (x2, y2 - box_h), cv2.FONT_HERSHEY_SIMPLEX, 0.5,color, 5) # Certeza de prediccion de la clase
+        frame = cv2.imencode('.jpg', Convertir_BGR(frame))[1].tobytes()
+        yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+            
+
+@app.route('/video_feed')
+def video_feed():
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
